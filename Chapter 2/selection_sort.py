256be281caf2f032145b319fdc65a387dc3ca11a
tianyuan233 <1406282384@qq.com>
2018-09-11 23:18:31 +0800

    选择排序法

---
+++ Chapter 2/selection_sort.py
@@ -0,0 +1,19 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([1,9,3,7,2]))
+
